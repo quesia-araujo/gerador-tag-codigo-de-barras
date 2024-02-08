e0194d2b5383b8bea5462704e3b90341c99fca61
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-02-08 15:54:17 -0400

    feat: Implementin View for tag creator with  Http Types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,16 @@
+from src.views.http_types.http_requests import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    '''
+        responsability for interacting with HTTP
+    '''
+    def validate_and_create(self,http_request: HttpRequest) -> HttpResponse:
+        #body = http_request.body
+        #product_code = body['product_code']
+
+        # logica de regra de negocio
+        print('Estou na minha view')
+        print(http_request)
+        # retorno http
+        return HttpResponse(status_code=200, body={"hello": "world"})
